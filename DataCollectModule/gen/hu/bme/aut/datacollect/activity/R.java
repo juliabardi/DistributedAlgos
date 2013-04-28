37bc26190d29c5910d867a156196126ab84e0b49
Eva Pataji <evapataji@gmail.com>
2013-04-28 20:28:07 +0200

    Added GCM push notification.

--- DataCollectModule/gen/hu/bme/aut/datacollect/activity/R.java
+++ DataCollectModule/gen/hu/bme/aut/datacollect/activity/R.java
@@ -60,10 +60,18 @@ public final class R {
         public static final int acc=0x7f060004;
         public static final int acceleration=0x7f060007;
         public static final int action_settings=0x7f060001;
+        public static final int already_registered=0x7f060015;
         public static final int app_name=0x7f060000;
         public static final int calls=0x7f06000b;
         public static final int communicationStop=0x7f060012;
+        public static final int error_config=0x7f060014;
         public static final int fine_location=0x7f06000a;
+        public static final int gcm_deleted=0x7f06001b;
+        public static final int gcm_error=0x7f060019;
+        public static final int gcm_message=0x7f060018;
+        public static final int gcm_recoverable_error=0x7f06001a;
+        public static final int gcm_registered=0x7f060016;
+        public static final int gcm_unregistered=0x7f060017;
         public static final int gyroscope=0x7f06000f;
         public static final int hello_world=0x7f060002;
         public static final int incall=0x7f06000c;
@@ -75,6 +83,11 @@ public final class R {
         public static final int outsms=0x7f060011;
         public static final int packages=0x7f060013;
         public static final int sensors=0x7f060003;
+        public static final int server_register_error=0x7f06001f;
+        public static final int server_registered=0x7f06001d;
+        public static final int server_registering=0x7f06001c;
+        public static final int server_unregister_error=0x7f060020;
+        public static final int server_unregistered=0x7f06001e;
         public static final int temp=0x7f060006;
         public static final int temperature=0x7f060009;
     }
--- DataCollectModule/src/hu/bme/aut/communication/CommunicationService.java
+++ DataCollectModule/src/hu/bme/aut/communication/CommunicationService.java
@@ -1,5 +1,10 @@
 package hu.bme.aut.communication;
 
+import static hu.bme.aut.communication.GCM.CommonUtilities.DISPLAY_MESSAGE_ACTION;
+import static hu.bme.aut.communication.GCM.CommonUtilities.EXTRA_MESSAGE;
+import static hu.bme.aut.communication.GCM.CommonUtilities.SENDER_ID;
+import static hu.bme.aut.communication.GCM.CommonUtilities.SERVER_URL;
+import hu.bme.aut.communication.GCM.ServerUtilities;
 import hu.bme.aut.datacollect.activity.MainActivity;
 import hu.bme.aut.datacollect.activity.R;
 
@@ -10,15 +15,26 @@ import org.json.JSONObject;
 
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.os.AsyncTask;
 import android.os.Binder;
 import android.os.IBinder;
 import android.preference.PreferenceManager;
 import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.android.gcm.GCMRegistrar;
 
 /**
+ * Main service which
+ * - sends messages to the node.js server via IntentService
+ * - registers and unregisters app to push notification service provided by GCM
+ * 
  * The service sends messages to the server if:
  * 	- there is a change in settings
  * When starting the service:
@@ -44,8 +60,8 @@ public class CommunicationService extends Service implements OnSharedPreferenceC
 	}
 	
 	// Server specific
-	private String serverAddress="http://10.0.2.2:3000/"; // Emulator localhost test
-	
+	private static String NodeServerAddress="http://10.0.2.2:3000/"; // Emulator localhost test
+	private static String TAG=CommunicationService.class.getSimpleName();
 	
 	
 	// Message specific, only these messages can be sent to the server
@@ -83,6 +99,7 @@ public class CommunicationService extends Service implements OnSharedPreferenceC
 		super.onCreate(); 
 		settings = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
 		settings.registerOnSharedPreferenceChangeListener(this);
+		registerGCM();
 		registerPeer();
 		setupForeground();
 	}
@@ -106,8 +123,16 @@ public class CommunicationService extends Service implements OnSharedPreferenceC
 
 	@Override
 	public void onDestroy() {
-		super.onDestroy();
 		settings.unregisterOnSharedPreferenceChangeListener(this);
+
+		if (mRegisterTask != null) {
+            mRegisterTask.cancel(true);
+        }
+		unregisterReceiver(mHandleMessageReceiver);
+		GCMRegistrar.unregister(this);         
+        GCMRegistrar.onDestroy(getApplicationContext()); // To handle exception error.
+        
+		super.onDestroy();
 	}
 	
 	/**
@@ -130,7 +155,7 @@ public class CommunicationService extends Service implements OnSharedPreferenceC
 		 JSONObject message = JsHelper.createMainBodyWithAlgos(ALGTYPE, 
 				 JsHelper.createAlgoBody(JsHelper.createSimpleArray(offerList),null));
 		
-		 sendJobToNodeService(serverAddress + REGISTER, message.toString());
+		 sendJobToNodeService(NodeServerAddress + REGISTER, message.toString());
 	}
 
 	/**
@@ -143,13 +168,13 @@ public class CommunicationService extends Service implements OnSharedPreferenceC
 		String url="";
 		if(value == true)
 		{
-			url=serverAddress +OFFER+"?"
+			url=NodeServerAddress +OFFER+"?"
 	  			   	+"algType="+ALGTYPE+"&"
 	  			   	+"name="+key;
 		}
 		else
 		{
-			url=serverAddress+UNREGISTER_OFFER+"?"
+			url=NodeServerAddress+UNREGISTER_OFFER+"?"
 	  			   	+"algType="+ALGTYPE+"&"
 	  			   	+"name="+key;
 		}
@@ -174,5 +199,87 @@ public class CommunicationService extends Service implements OnSharedPreferenceC
 	{
 		handleOffer(key,sharedPrefs.getBoolean(key,false));
 	}
+	
+	
+	// GCM - specific
+	
+	AsyncTask<Void, Void, Void> mRegisterTask;
+	/**
+	 * Registering to the GCM push notification service.
+	 */
+	private void registerGCM()
+	{
+		checkNotNull(SERVER_URL, "SERVER_URL");
+        checkNotNull(SENDER_ID, "SENDER_ID");
+        // Make sure the device has the proper dependencies.
+        GCMRegistrar.checkDevice(this);
+        // Make sure the manifest was properly set - comment out this line
+        // while developing the app, then uncomment it when it's ready.
+        GCMRegistrar.checkManifest(this);
+        
+        
+        registerReceiver(mHandleMessageReceiver,
+                new IntentFilter(DISPLAY_MESSAGE_ACTION));
+        final String regId = GCMRegistrar.getRegistrationId(this);
+        if (regId.equals("")) {
+            // Automatically registers application on startup.
+            GCMRegistrar.register(this, SENDER_ID);
+        } else {
+            // Device is already registered on GCM, check server.
+            if (GCMRegistrar.isRegisteredOnServer(this)) {
+                // Skips registration.
+                Log.i(TAG, "Already registered.");
+            } else {
+                // Try to register again, but not in the UI thread.
+                // It's also necessary to cancel the thread onDestroy(),
+                // hence the use of AsyncTask instead of a raw thread.
+                final Context context = this;
+                mRegisterTask = new AsyncTask<Void, Void, Void>() {
+
+                    @Override
+                    protected Void doInBackground(Void... params) {
+                        boolean registered =
+                                ServerUtilities.register(context, regId);
+                        // At this point all attempts to register with the app
+                        // server failed, so we need to unregister the device
+                        // from GCM - the app will try to register again when
+                        // it is restarted. Note that GCM will send an
+                        // unregistered callback upon completion, but
+                        // GCMIntentService.onUnregistered() will ignore it.
+                        if (!registered) {
+                            GCMRegistrar.unregister(context);
+                        }
+                        return null;
+                    }
+
+                    @Override
+                    protected void onPostExecute(Void result) {
+                        mRegisterTask = null;
+                    }
+
+                };
+                mRegisterTask.execute(null, null, null);
+            }
+        }    
+
+	}
+	
+	 private void checkNotNull(Object reference, String name) {
+	        if (reference == null) {
+	            throw new NullPointerException(
+	                    getString(R.string.error_config, name));
+	        }
+	 }
+
+	    private final BroadcastReceiver mHandleMessageReceiver =
+	            new BroadcastReceiver() {
+	        @Override
+	        public void onReceive(Context context, Intent intent) {
+	            String newMessage = intent.getExtras().getString(EXTRA_MESSAGE);
+	           Log.i(TAG,newMessage);
+	        }
+	    };
+
+	
 
 }
