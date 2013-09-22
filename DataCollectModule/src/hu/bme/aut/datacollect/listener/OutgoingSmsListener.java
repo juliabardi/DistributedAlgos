b429f40b82726db97446ec313f7137d9165e3dc2
Bardi Julia <bardi.julia@gmail.com>
2013-09-22 11:45:20 +0200

    Megjavítottam az sms küldés figyelést.

--- DataCollectModule/src/hu/bme/aut/datacollect/listener/OutgoingSmsListener.java
+++ DataCollectModule/src/hu/bme/aut/datacollect/listener/OutgoingSmsListener.java
@@ -8,6 +8,7 @@ import java.util.Calendar;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.database.ContentObserver;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Handler;
 import android.telephony.TelephonyManager;
@@ -18,21 +19,37 @@ public class OutgoingSmsListener extends ContentObserver implements IListener {
 	private Context context = null;
 	
 	private boolean regOutSms = false;
+	
+	private int lastId = 0;
 		
 	public OutgoingSmsListener(Context context, Handler handler, DaoBase<SmsData> smsDao){
 		super(handler);
 		this.smsDao = smsDao;
 		this.context = context;
 	}
-
-	@Override
-	public void onChange(boolean selfChange) {		
-		this.onChange(selfChange, null);
-	}	
 	
 	@Override
 	public void onChange(boolean selfChange, Uri uri) {
-		smsDao.create(new SmsData(Calendar.getInstance().getTimeInMillis(), "out"));
+		
+        Uri uriSMSURI = Uri.parse("content://sms");
+        Cursor cur = context.getContentResolver().query(uriSMSURI, null, null,
+             null, null);
+        cur.moveToNext();
+        
+        if (this.lastId != cur.getInt(cur.getColumnIndex("_id"))){
+        	//last id is needed because the method gets called 3 times
+        	this.lastId = cur.getInt(cur.getColumnIndex("_id"));
+	        String protocol = cur.getString(cur.getColumnIndex("protocol"));
+	
+	        if(protocol == null) {
+	        	//the message is sent out just now  
+	    		smsDao.create(new SmsData(Calendar.getInstance().getTimeInMillis(), "out"));
+	        }               
+	        else {
+	             //the message is received just now   
+	        	 //this is handled with IncomingSmsReceiver
+	        }
+        }
 	}
 
 	@Override
@@ -41,7 +58,7 @@ public class OutgoingSmsListener extends ContentObserver implements IListener {
 		if (!regOutSms) {
 			ContentResolver contentResolver = context.getContentResolver();
 			contentResolver.registerContentObserver(
-					Uri.parse("content://sms/out"), true, this);
+					Uri.parse("content://sms"), true, this);
 			regOutSms = true;
 		}
 	}
