8f5ac8b6a3a2d5d20a57ca466bd0a47c26996662
Bardi Julia <bardi.julia@gmail.com>
2013-10-29 18:08:12 +0100

    Kiszedtem a header-ös részeket a settingsből, mert már nem kellenek.

--- DataCollectModule/src/hu/bme/aut/datacollect/activity/ActivityFragmentSettings.java
+++ DataCollectModule/src/hu/bme/aut/datacollect/activity/ActivityFragmentSettings.java
@@ -1,14 +1,10 @@
 package hu.bme.aut.datacollect.activity;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-import android.annotation.TargetApi;
-import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
-import android.preference.PreferenceFragment;
 
 public class ActivityFragmentSettings extends PreferenceActivity {
 	
@@ -16,7 +12,6 @@ public class ActivityFragmentSettings extends PreferenceActivity {
 	final static String ACTION_PREFS_LOCATION = "hu.bme.aut.datacollect.prefs.PREFS_LOCATION";
 	final static String ACTION_PREFS_CALLS = "hu.bme.aut.datacollect.prefs.PREFS_CALLS";
 	final static String ACTION_PREFS_SMS = "hu.bme.aut.datacollect.prefs.PREFS_SMS";
-	final static String ACTION_PREFS_SERVERS = "hu.bme.aut.datacollect.prefs.PREFS_SERVERS";
 	
 	static Map<String, Boolean> availableListeners = new HashMap<String, Boolean>();
 	
@@ -64,9 +59,6 @@ public class ActivityFragmentSettings extends PreferenceActivity {
 	        	this.findPreference(DataCollectService.SMS).setEnabled(false);
 	        } 
 	    }
-	    else if (action != null && action.equals(ACTION_PREFS_SERVERS)){
-	    	addPreferencesFromResource(R.xml.serversettings);
-	    }
 	    else {
 	    	addPreferencesFromResource(R.xml.preference_headers_legacy);
 	    }
@@ -87,60 +79,4 @@ public class ActivityFragmentSettings extends PreferenceActivity {
 		super.onDestroy();
 		ActivityFragmentSettings.instance = null;
 	}
-
-	@TargetApi(Build.VERSION_CODES.HONEYCOMB) 
-	@Override
-	public void onBuildHeaders(List<Header> target) {
-		//giving up on fragments, because cannot mix headers with normal preferences
-		//loadHeadersFromResource(R.xml.fragmentsettings, target);
-	}
-	
-	@TargetApi(Build.VERSION_CODES.HONEYCOMB) 
-    public static class FragmentSettingsSensors extends PreferenceFragment {
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            addPreferencesFromResource(R.xml.sensorsettings); 
-            if (!availableListeners.get(DataCollectService.ACCELERATION)){
-            	this.findPreference(DataCollectService.ACCELERATION).setEnabled(false);
-            }
-            if (!availableListeners.get(DataCollectService.LIGHT)){
-            	this.findPreference(DataCollectService.LIGHT).setEnabled(false);
-            }
-            if (!availableListeners.get(DataCollectService.TEMPERATURE)){
-            	this.findPreference(DataCollectService.TEMPERATURE).setEnabled(false);
-            }
-            if (!availableListeners.get(DataCollectService.GYROSCOPE)){
-            	this.findPreference(DataCollectService.GYROSCOPE).setEnabled(false);
-            }
-            if (!availableListeners.get(DataCollectService.PROXIMITY)){
-            	this.findPreference(DataCollectService.PROXIMITY).setEnabled(false);
-            }
-        }
-    }
-	
-	@TargetApi(Build.VERSION_CODES.HONEYCOMB) 
-    public static class FragmentSettingsLoc extends PreferenceFragment {
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            addPreferencesFromResource(R.xml.locsettings);
-            if (!availableListeners.get(DataCollectService.LOCATION)){
-            	this.findPreference(DataCollectService.LOCATION).setEnabled(false);
-            }
-        }
-    }
-	
-	@TargetApi(Build.VERSION_CODES.HONEYCOMB) 
-    public static class FragmentSettingsCalls extends PreferenceFragment {
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            addPreferencesFromResource(R.xml.callsettings);
-            if (!availableListeners.get(DataCollectService.CALL)){
-            	this.findPreference(DataCollectService.CALL).setEnabled(false);
-            	this.findPreference(DataCollectService.SMS).setEnabled(false);
-            }
-        }
-    }
 }
